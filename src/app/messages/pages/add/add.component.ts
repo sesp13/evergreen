029d9c26ae3c928cebf1e80c851f8d9ecbe12987
sesp13 <sespinosarte99@gmail.com>
2022-03-23 16:33:12 -0500

    use toastr when a message is created or edited

--- src/app/messages/pages/add/add.component.ts
+++ src/app/messages/pages/add/add.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Message } from '../../interfaces/message.interface';
 import { MessageService } from '../../services/message.service';
 import { switchMap } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add',
@@ -13,6 +14,7 @@ import { switchMap } from 'rxjs/operators';
 export class AddComponent implements OnInit {
   title: string = '';
   submitTitle: string = '';
+  successMessage: string = '';
 
   form: FormGroup = this.fb.group({
     content: ['', [Validators.required]],
@@ -26,16 +28,16 @@ export class AddComponent implements OnInit {
     private messageService: MessageService,
     private fb: FormBuilder,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
     if (this.router.url.includes('edit')) {
-      this.title = `Editar mensaje`;
-      this.submitTitle = 'Editar';
+      this.setUpLabels(true);
       this.route.params
-      .pipe(
-        switchMap(({ id }) => this.messageService.getMessageById(id ?? ''))
+        .pipe(
+          switchMap(({ id }) => this.messageService.getMessageById(id ?? ''))
         )
         .subscribe((message: Message) => {
           this.form.setValue({
@@ -47,8 +49,19 @@ export class AddComponent implements OnInit {
           });
         });
     } else {
+      this.setUpLabels(false);
+    }
+  }
+
+  setUpLabels(edit: boolean): void {
+    if (edit) {
+      this.title = `Editar mensaje`;
+      this.submitTitle = 'Editar';
+      this.successMessage = 'Mensaje editado correctamente!';
+    } else {
       this.title = 'Crear Mensaje';
       this.submitTitle = 'Guardar';
+      this.successMessage = 'Mensaje creado correctamente!';
     }
   }
 
@@ -58,13 +71,14 @@ export class AddComponent implements OnInit {
       this.messageService
         .updateMessage(this.form.value as Message)
         .subscribe((result) => {
-          console.log(result);
+          this.toastr.success(this.successMessage);
         });
     } else {
       // Save
       this.messageService
         .saveMessage(this.form.value as Message)
         .subscribe((result: Message) => {
+          this.toastr.success(this.successMessage);
           this.router.navigate(['/messages/edit', result?.id]);
         });
     }
