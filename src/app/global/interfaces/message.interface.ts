644e7376621556a28870fec911e84b04efd4fe22
sesp13 <sespinosarte99@gmail.com>
2022-03-25 18:26:31 -0500

    add users fills on send table

--- src/app/global/interfaces/message.interface.ts
+++ src/app/global/interfaces/message.interface.ts
@@ -1,8 +1,12 @@
+import { User } from "./user.interface";
+
 export interface Message {
   subject?: string;
   content?: string;
   sender?: string;
+  senderObject?: User;
   receivers?: string[];
+  receiversObjects?: User[];
   status?: string;
   id?: string;
 }
--- src/app/send/pages/list/list.component.ts
+++ src/app/send/pages/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Message } from 'src/app/global/interfaces/message.interface';
+import { User } from 'src/app/global/interfaces/user.interface';
 import { SendService } from 'src/app/global/services/send.service';
 import { UserService } from 'src/app/global/services/user.service';
 
@@ -23,11 +24,21 @@ export class ListComponent implements OnInit {
   getMessages(): void {
     this.sendService.getMessages().subscribe((result: Message[]) => {
       this.messagesLst = result;
+      this.setUsers();
+    });
+  }
 
+  setUsers(): void {
+    this.messagesLst.forEach((message: Message) => {
+      // Sender
+      this.userService.getUserById(message?.id).subscribe((user: User) => {
+        message.senderObject = user;
+      });
+      // Receivers
       this.userService
-        .getUsersByIds(this.messagesLst[0].receivers)
-        .subscribe((result) => {
-          console.log(result);
+        .getUsersByIds(message.receivers)
+        .subscribe((result: User[]) => {
+          message.receiversObjects = result;
         });
     });
   }
